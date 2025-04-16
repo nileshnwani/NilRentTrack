cb3a3a7250598dc245b05c733af5f63694d28c29
Nilesh Wani <108217005+nileshnwani@users.noreply.github.com>
2025-04-17 01:25:38 +0530

    Delete app/api/admin directory

--- app/api/admin/dashboard/route.js
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-dynamic';
-import { checkRole } from '@/middleware/authorization';
-
-export async function GET(req) {
-  const authResult = checkRole(req, ['admin']);
-  if (authResult instanceof NextResponse) return authResult; // Return error if not admin
-
-  return NextResponse.json({ message: 'Welcome to Admin Dashboard' });
-}
